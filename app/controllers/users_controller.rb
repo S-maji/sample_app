09f943d4dcb5bb1cd289e654b986b9ede57df3e1
EC2 Default User <ec2-user@ip-172-31-6-44.us-east-2.compute.internal>
2018-02-12 20:09:29 +0000

    Finish user destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, :except=>[:show]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -8,4 +9,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+  
 end
