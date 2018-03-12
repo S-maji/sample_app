89cf9951bab75c929a658dc69bc92de8c58c684f
EC2 Default User <ec2-user@ip-172-31-6-44.us-east-2.compute.internal>
2018-03-12 20:41:08 +0000

    info

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  get "contacts/new"
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
@@ -10,6 +11,7 @@ SampleApp::Application.routes.draw do
 
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :contacts, only: [:new, :create]
   
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
